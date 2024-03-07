b620a3534d916268e000b797666673fbed354692
Рамазан <kemelovramazan@gmail.com>
2024-03-07 11:14:32 +0600

    add get credits from api

--- credit-table/src/App.vue
+++ credit-table/src/App.vue
@@ -1,31 +1,50 @@
 
 <template>
   <div>
-    <DataTable :value="products" ref="dt" tableStyle="min-width: 50rem">
+    <DataTable :value="credits" ref="dt" paginator :rows="100" tableStyle="min-width: 50rem">
       <template #header>
         <div style="text-align: left">
           <Button icon="pi pi-external-link" label="Export" @click="exportCSV($event)" />
         </div>
       </template>
-      <Column field="code" header="Code" exportHeader="Product Code"></Column>
-      <Column field="name" header="Name"></Column>
-      <Column field="category" header="Category"></Column>
-      <Column field="quantity" header="Quantity"></Column>
+      <Column field="id" header="ID" exportHeader="Credit ID"></Column>
+      <Column field="createdDate" header="Created Date"></Column>
+      <Column field="status" header="Status"></Column>
+      <Column field="amount" header="Amount"></Column>
+      <Column field="openDate" header="Open Date"></Column>
+      <Column field="closeDate" header="Close Date"></Column>
+      <Column field="customerId" header="Customer ID"></Column>
     </DataTable>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { ProductService } from '@/service/ProductService';
-
+import axios from 'axios';
+import { useToast } from 'primevue/usetoast';
+const toast = useToast();
 onMounted(() => {
-  ProductService.getProductsMini().then((data) => (products.value = data));
+  getUsers();
 });
 
 const dt = ref();
-const products = ref();
+const credits = ref();
 const exportCSV = () => {
   dt.value.exportCSV();
+};
+
+const getUsers = async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/api/credits');
+    credits.value = response.data;
+  } catch (error) {
+    console.error('Ошибка при загрузке данных:', error);
+    toast.add({
+      severity: 'error',
+      summary: 'Ошибка',
+      detail: 'Не удалось загрузить кредиты',
+      life: 3000,
+    });
+  }
 };
 </script>
